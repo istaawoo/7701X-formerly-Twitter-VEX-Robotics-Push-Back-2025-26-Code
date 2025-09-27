dd1f4f54dec783fa4af0dfff793c4d38dd418d8b
“Noah <noahsamanderson@gmail.com>
2025-09-27 11:22:50 -0400

    pid

---
+++ 7701X_Push_Back/src/pid.cpp
@@ -0,0 +1,34 @@
+//include main
+#include "main.h" 
+
+//include pros files
+#include "liblvgl/llemu.h" // IWYU pragma: keep
+#include "liblvgl/llemu.hpp"
+#include "pros/abstract_motor.hpp" // IWYU pragma: keep
+#include "pros/adi.h" // IWYU pragma: keep
+#include "pros/adi.hpp" // IWYU pragma: keep
+#include "pros/colors.hpp" // IWYU pragma: keep
+#include "pros/device.hpp" // IWYU pragma: keep
+#include "pros/distance.hpp" // IWYU pragma: keep
+#include "pros/imu.h"  // IWYU pragma: keep
+#include "pros/imu.hpp" // IWYU pragma: keep
+#include "pros/llemu.hpp"
+#include "pros/misc.h"
+#include "pros/motors.h" // IWYU pragma: keep
+#include "pros/motors.hpp" // IWYU pragma: keep
+#include "pros/rotation.hpp" // IWYU pragma: keep
+#include "pros/rtos.hpp"
+#include "pros/screen.hpp" // IWYU pragma: keep
+
+//general C++ utilities 
+#include <cmath>
+#include <iostream> // IWYU pragma: keep
+#include <stdio.h> // IWYU pragma: keep
+
+//include PID/movement utilities
+#include "pid.hpp"
+#include "mcl.cpp"
+#include "mcl.hpp"
+#include "auton.hpp"
+
+class PID (float P_value = 0, )
