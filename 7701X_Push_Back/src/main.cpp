d073c89a8606e64e92b951c7e24f4c61f278dd38
Jacob <jacobbeardsley118@gmail.com>
2025-10-23 15:25:26 -0400

    Yah

--- 7701X_Push_Back/src/main.cpp
+++ 7701X_Push_Back/src/main.cpp
@@ -49,26 +49,28 @@ MotorGroup left_motors({-11, -12, -13}, MotorCartridge::blue);
 MotorGroup intake1({-1}, MotorCartridge::blue);
 MotorGroup intake2({-21}, MotorCartridge::blue);
 
+//Initializes IMUs and Distance sensors
+Imu imu1(8);
+Imu imu2(2);
+
+Distance right(3);
+Distance front(4);
+Distance left(5);
+Distance back(6);
+
 Robot robot(11.0, 10.0, .75, 3.25, 5.75, &right_motors, &left_motors, LatPID::lat_one, TurnPID::turn_one);
 
 Controller controller(pros::E_CONTROLLER_MASTER);
 
 void initialize() { // runs initialization; keep execution time under three seconds
-    //Initializes IMUs and Distance sensors and adds them to the robot sensor vectors
-    Imu imu1(8);
-    robot.imus.push_back(std::make_unique<Imu>(1));
-    Imu imu2(2);
+    //Adds sensors to the robot sensor vectors
+    robot.imus.push_back(std::make_unique<Imu>(8));
     robot.imus.push_back(std::make_unique<Imu>(2));
-
-    Distance right(3);
-    robot.distances.push_back(std::make_unique<Distance>(3));
-    Distance front(4);
-    robot.distances.push_back(std::make_unique<Distance>(4));
-    Distance left(5);
-    robot.distances.push_back(std::make_unique<Distance>(5));
-    Distance back(6);
-    robot.distances.push_back(std::make_unique<Distance>(6));
-
+    robot.mclDistances.push_back(std::make_unique<Distance>(3));
+    robot.mclDistances.push_back(std::make_unique<Distance>(4));
+    robot.mclDistances.push_back(std::make_unique<Distance>(5));
+    robot.mclDistances.push_back(std::make_unique<Distance>(6));
+    
     //calibrate IMUs
     imu1.reset();
     imu2.reset();
@@ -103,21 +105,8 @@ void opcontrol() {
 		int rightStickX = controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_X);
 		int leftStickY = controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
 
-		if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {shift = true;} else {shift = false;}
-
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
-            if (!shift) {
-                intake1.move_voltage(-12000); // 
-                intake2.move_voltage(12000);
-            } else {
-                intake1.move_voltage(-12000); //
-                intake2.move_voltage(-12000);
-            }
-        } else if (!controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
-                intake1.brake();
-                intake2.brake();
-        }
-
+		shift = controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2);
+        
         if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
             if (!shift) {
                 intake1.move_voltage(12000);
@@ -131,6 +120,19 @@ void opcontrol() {
                 intake1.brake();
                 intake2.brake();
             }
+        }
+
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            if (!shift) {
+                intake1.move_voltage(-12000); // 
+                intake2.move_voltage(12000);
+            } else {
+                intake1.move_voltage(-12000); //
+                intake2.move_voltage(-12000);
+            }
+        } else if (!controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+                intake1.brake();
+                intake2.brake();
         }
 
 		left_motors.move(leftStickY + rightStickX);
